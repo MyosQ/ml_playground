b41b7a2781d53401eee872ecb7a5345bc2941236
MyosQ <frej@capia.no>
2022-08-28 13:47:40 +0200

    test3

--- chapter2.py
+++ chapter2.py
@@ -83,4 +83,4 @@ housing.dropna(subset=["total_bedrooms"]) # option 1
 housing.drop("total_bedrooms", axis=1) # option 2
 median = housing["total_bedrooms"].median() # option 3
 housing["total_bedrooms"].fillna(median, inplace=True)
-#
+##
